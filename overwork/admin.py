88d58e4186aeeea4abc5d6e4ba2a334be5233dfc
anokata <anokata8@mail.ru>
2017-05-10 11:47:49 +0300

    develop models. add overworks. migrate

--- overwork/admin.py
+++ overwork/admin.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Person, Overs
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Overs)
--- overwork/models.py
+++ overwork/models.py
@@ -7,3 +7,28 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=200)
     is_manager = models.BooleanField()
+
+    def __str__(self):
+        return self.name  + (' (+)' if self.is_manager else ' (-)')
+
+
+class Overs(models.Model):
+    STATUS_CHOISES = (
+        ('R', 'Registred'),
+        ('D', 'Denied'),
+        ('A', 'Accepted'),
+    )
+    reg_date = models.DateTimeField('date registred')
+    start_date = models.DateTimeField('start date')
+    interval = models.IntegerField()
+    status = models.CharField(max_length=100,
+                                  choices=STATUS_CHOISES,
+                                  default='R')
+    comment = models.CharField(max_length=512)
+    # ref on person
+
+    def __str__(self):
+        return self.reg_date.strftime("%Y.%m.%d") + ' ' \
+                + self.get_status_display() + '  ' \
+                + str(self.interval) + ' hours'
+
