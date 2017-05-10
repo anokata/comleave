2c322967a49242fc65bc222d659d0d695de32a75
anokata <anokata8@mail.ru>
2017-05-10 12:59:24 +0300

    try make views and template. need make rest for angular

--- overwork/urls.py
+++ overwork/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.test, name='test'),
+    url(r'^persons$', views.persons, name='persons'),
 ]
--- overwork/views.py
+++ overwork/views.py
@@ -3,8 +3,18 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-# Create your views here.
+from .models import Overs, Person
 
 def test(request):
     return HttpResponse("hi")
+
+def persons(request):
+    persons = Person.objects.order_by('name')
+    template = loader.get_template('overwork/index.html')
+    context = {
+        'persons': persons,
+    }
+    return HttpResponse(template.render(context))
+
