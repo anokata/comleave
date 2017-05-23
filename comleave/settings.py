aa8b37863f63594ffb14e54759996d7c8a9beed1
anokata <anokata8@mail.ru>
2017-05-23 12:22:44 +0300

    uwsgi and nginx prod config

--- comleave/settings.py
+++ comleave/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/ng/'
-STATIC_ROOT = BASE_DIR + 'ng/'
+STATIC_ROOT = BASE_DIR + '/ng/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "front"),
 )
