529aac454d195cf905463827d8aceda3ac363fea
anokata <anokata8@mail.ru>
2017-05-29 12:42:50 +0300

    Making tests

--- overwork/tests.py
+++ overwork/tests.py
@@ -2,10 +2,32 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from .models import Overs, Person
 
 class SimpleTest(TestCase):
-    def test_details(self):
-        print('test view')
+    def test_root(self):
+        print('*** Test view')
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         print('test view ok')
+
+    def test_manager(self):
+        print('*** Test persons')
+        persons = Person.objects.all()
+        self.assertEqual(len(persons), 1, "manager not created")
+        print(persons[0])
+
+    def test_registration(self):
+        print('*** Test registration')
+        response = self.client.post('/register_user/', {
+            'username':'testuser',
+            'password':'1',
+            'email':'email@mail.com',
+            'first_name':'first_name',
+            'last_name':'last_name',
+                    })
+        print(response)
+        persons = Person.objects.all()
+        self.assertEqual(len(persons), 2, "test user not created")
+        print(persons[1])
+
